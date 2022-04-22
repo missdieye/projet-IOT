f990e5e04cd11ecd375b3abbbc5bde6f3c4db9bf
missdieye <khadijadieye@gmail.com>
2022-04-22 02:00:43 +0200

    change

--- ui_lucioles.js
+++ ui_lucioles.js
@@ -107,7 +107,7 @@ function get_samples(path_on_node, serie, wh) {
 	// wh => which esp do we want to query data
 
 	// node_url = "http://localhost:3000";
-	node_url = "https://iot22112951m1.herokuapp.com/";
+	node_url = "https://iot22112951m1.herokuapp.com";
 	//node_url = 'http://192.168.1.101:3000'
 
 	//https://openclassrooms.com/fr/courses/1567926-un-site-web-dynamique-avec-jquery/1569648-le-fonctionnement-de-ajax
