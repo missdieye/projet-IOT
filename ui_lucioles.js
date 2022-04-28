550f5be5a581767eb8bde0be652c68ed5d7a4761
missdieye <khadijadieye@gmail.com>
2022-04-28 22:56:57 +0200

    localhost delete

--- ui_lucioles.js
+++ ui_lucioles.js
@@ -134,8 +134,8 @@ function get_samples(path_on_node, serie, wh) {
 	});
 }
 function processAuthorizedESP(path_on_node) {
-	// node_url = "https://iot22112951m1.herokuapp.com";
-	node_url = "http://localhost:3000";
+	node_url = "https://iot22112951m1.herokuapp.com";
+	// node_url = "http://localhost:3000";
 	$.ajax({
 		url: node_url.concat(path_on_node), // URL to "GET" : /connectedESP
 		type: "GET",
