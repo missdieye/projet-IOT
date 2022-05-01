11e5ec83ab5b6949126c71eb7b150a0430d7af56
missdieye <khadijadieye@gmail.com>
2022-05-01 18:04:27 +0200

    last_commit

--- Leaflet_esp/maps/static_markers.js
+++ Leaflet_esp/maps/static_markers.js
@@ -1,28 +1,57 @@
-var node_url = "https://iot22112951m1.herokuapp.com";
-$.ajax({
-	url: node_url.concat("/coordonnees"), // URL to "GET" : /connectedESP
-	type: "GET",
-	headers: { Accept: "application/json" },
-	success: function (resultat) {
-		resultat.forEach(element => {
-			mylocation_lon = element.longitude; // lon WGS84
-			mylocation_lat = element.lattitude;
-			var myURL = jQuery('script[src$="static_markers.js"]').attr("src").replace("static_markers.js", "");
+// var node_url = "https://iot22112951m1.herokuapp.com";
+var node_url = "http://localhost:3000";
+//Pour ajouter dans la carte les données des ESP autorisés
+setInterval(
+	$.ajax({
+		url: node_url.concat("/coordonnees"), // URL to "GET" : /connectedESP
+		type: "GET",
+		headers: { Accept: "application/json" },
+		success: function (resultat) {
+			resultat.forEach(element => {
+				// Pour récupérer la température des ESP autorisés
+				$.ajax({
+					url: node_url.concat("/esp/temp"), // URL to "GET" : /esp/temp ou /esp/light
+					type: "GET",
+					headers: { Accept: "application/json" },
+					data: { who: element.macEsp }, // parameter of the GET request
+					success: function (resultat2, statut) {
+						console.log("resultat2", resultat2);
+						var temp = "ESP non connecté";
+						resultat2.forEach(element2 => {
+							if (element2.who === element.macEsp) {
+								temp = resultat2[0].value;
+							}
+						});
 
-			const myIcon = L.icon({
-				iconUrl: myURL + "images/pin24.png",
-				iconRetinaUrl: myURL + "images/pin48.png",
-				iconSize: [29, 24],
-				iconAnchor: [9, 21],
-				popupAnchor: [0, -14]
+						mylocation_lon = element.longitude; // lon WGS84
+						mylocation_lat = element.lattitude;
+						var myURL = jQuery('script[src$="static_markers.js"]').attr("src").replace("static_markers.js", "");
+
+						const myIcon = L.icon({
+							iconUrl: myURL + "images/pin24.png",
+							iconRetinaUrl: myURL + "images/pin48.png",
+							iconSize: [29, 24],
+							iconAnchor: [9, 21],
+							popupAnchor: [0, -14]
+						});
+						L.marker([mylocation_lat, mylocation_lon], {
+							icon: myIcon
+						})
+							.bindPopup(
+								"<b>MAC ESP</b> =   " +
+									element.macEsp +
+									"<br> <b>User</b> = " +
+									element.username +
+									"<br> <b>Température</b> = " +
+									temp
+							)
+							.addTo(map);
+					}
+				});
 			});
-			L.marker([mylocation_lat, mylocation_lon], {
-				icon: myIcon
-			})
-				.bindPopup("<b>adress ESP</b> =   " + element.macEsp + "<br> <b>Nom du user</b> = " + element.username)
-				.addTo(map);
-		});
-	},
-	error: function (resultat, statut, erreur) {},
-	complete: function (resultat, statut) {}
-});
+		},
+		error: function (resultat, statut, erreur) {},
+		complete: function (resultat, statut) {}
+	}),
+	5000
+);
